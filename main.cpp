4c2ad4d73be939417cddf5bd2b1233b13975a9aa
Piyush Mohite <piyushm17mar@gmail.com>
2022-10-21 19:07:21 +0530

    feat(*): executable cmake functioning
    Unable to find image

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@
 
 using namespace cv;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     Mat image;
-    image = imread("test.gif", CV_LOAD_IMAGE_COLOR);   // Read the file
+    image = imread("test.jpg"); // Read the file
 
-    if(! image.data )                              // Check for invalid input
+    if (!image.data) // Check for invalid input
     {
-        std::cout <<  "Could not open or find the image" << std::endl;
+        std::cout << "Could not open or find the image" << std::endl;
         return -1;
     }
 
-    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
-    imshow( "Display window", image );                   // Show our image inside it.
+    namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+    imshow("Display window", image);                // Show our image inside it.
 
-    waitKey(0);                                          // Wait for a keystroke in the window
+    waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
