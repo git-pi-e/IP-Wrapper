60ba0668b758c7d5aa7ab248152120eee16a751d
Harsh Gujarathi <f20201712@goa.bits-pilani.ac.in>
2022-12-09 20:06:14 +0530

    corrected

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/harsh/Desktop/IP-Wrapper
+# Install script for directory: /home/harsh/Desktop/popl/build/check/IP-Wrapper
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "0")
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/harsh/Desktop/IP-Wrapper/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/harsh/Desktop/popl/build/check/IP-Wrapper/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
